5335f06b8428feb355d14b81ada5e4d3979a14d7
Simon Krempin <67335158+simonkrempin@users.noreply.github.com>
2024-06-12 14:29:30 +0200

    #3: base page layout

---
+++ src/routes/(application)/learn/+page.svelte
@@ -0,0 +1,91 @@
+<script lang="ts">
+	let showSolution = false;
+
+	function onSolutionWrong() {
+		showSolution = false;
+	}
+
+	function onSolutionCorrect() {
+		showSolution = false;
+	}
+</script>
+
+<section>
+    <button
+            class="unknown_button"
+            on:click={onSolutionWrong}
+    >
+        X
+    </button>
+    <div class="main_content">
+        <div>
+            <h3>Das hier ist die Frage</h3>
+        </div>
+        {#if showSolution}
+            <div>
+                <p>X</p>
+            </div>
+        {/if}
+        <button on:click={() => showSolution = !showSolution}>
+            {showSolution ? "Verstecke die Lösung" : "Zeige die Lösung an"}
+        </button>
+    </div>
+    <button
+            class="unknown_button"
+            on:click={onSolutionCorrect}
+    >
+        C
+    </button>
+</section>
+
+<style>
+    section {
+        flex: 1;
+        width: 100%;
+        display: flex;
+        padding: 10% 5%;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    div {
+        flex: 1;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .main_content {
+        width: 100%;
+        height: 100%;
+        border-radius: 24px;
+        background-color: #f5f5f5;
+        margin: 0 5%;
+        padding: 24px;
+        flex-direction: column;
+        justify-content: end;
+    }
+
+    button {
+        width: fit-content;
+        padding: 8px 20px;
+        border-radius: 100px;
+        border: none;
+        background-color: #eee;
+    }
+
+    .unknown_button {
+        background-color: #f5f5f5;
+        height: 50px;
+        width: 50px;
+        border: none;
+        border-radius: 50%;
+        padding: 0;
+    }
+
+    button:hover {
+        background-color: #b4b4b4;
+        transform: scale(1.05);
+    }
+</style>
